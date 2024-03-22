9bd7dd3323afdfa9b0974cbf73f25f82a5a308a5
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-22 17:02:39 +0200

    feat: :sparkles: separate menu in new component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">
+          Mohamed Omar
+        </NuxtLink>
+      </div>
+
+      <Menu />
+    </header>
+
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
-
-<script setup></script>
