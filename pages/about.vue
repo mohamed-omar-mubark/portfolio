2b9c83d273bc65f3d0d0dedb0209f16cc423ef96
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-22 19:02:05 +0200

    feat: :sparkles: SEO, Meta tags and custom font family

--- pages/about.vue
+++ pages/about.vue
@@ -2,8 +2,17 @@
   <div>about page</div>
 </template>
 
-<script>
-export default {};
+<script setup>
+useSeoMeta({
+  title: "About",
+  description: "About page",
+  ogTitle: "About",
+  ogDescription: "About page",
+  ogImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
+  ogUrl: "https://mohamed-omar.com",
+  twitterTitle: "About",
+  twitterDescription: "About page",
+  twitterImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
+  twitterCard: "summary",
+});
 </script>
-
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,30 @@
   <div>Hello Vue</div>
 </template>
 
-<script setup></script>
+<script setup>
+useSeoMeta({
+  title: "Portfolio",
+  description: "This is my portfolio",
+  ogTitle: "Portfolio",
+  ogDescription: "This is my portfolio",
+  ogImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
+  ogUrl: "https://mohamed-omar.com",
+  twitterTitle: "Portfolio",
+  twitterDescription: "This is my portfolio",
+  twitterImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
+  twitterCard: "summary",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
+</script>
