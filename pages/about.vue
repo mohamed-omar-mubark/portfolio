8bc0ab67589c8959ecbfbd40c0a55b25447a47e6
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-22 21:14:59 +0200

    feat: :sparkles: custom pages in markdown and typograhpy customization

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,5 @@
 <template>
-  <div>about page</div>
+  <article class="prose dark:prose-invert prose-h1:text-2xl">
+    <ContentDoc path="/about" />
+  </article>
 </template>
-
-<script setup>
-useSeoMeta({
-  title: "About",
-  description: "About page",
-  ogTitle: "About",
-  ogDescription: "About page",
-  ogImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
-  ogUrl: "https://mohamed-omar.com",
-  twitterTitle: "About",
-  twitterDescription: "About page",
-  twitterImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
-  twitterCard: "summary",
-});
-</script>
