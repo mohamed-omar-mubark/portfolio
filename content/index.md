aae489b8f7f75d668bfdc84093b2f077c22a071b
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2024-03-24 22:32:27 +0200

    feat: :sparkles: Main page, props in MDC, Reusing the blog posts component

---
+++ content/index.md
@@ -0,0 +1,14 @@
+---
+title: "Main"
+description: "This is main page of my portfolio"
+---
+
+# Hello
+
+## I'm Mohamed Omar
+
+Read more [about me](/about), or see my latest [projects](/projects)
+
+## Latest on my blog
+
+::latest-blog-posts{:limit=3}
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,5 @@
 <template>
-  <div>Hello Vue</div>
+  <article class="prose dark:prose-invert max-w-none">
+    <ContentDoc path="/" />
+  </article>
 </template>
-
-<script setup>
-useSeoMeta({
-  title: "Portfolio",
-  description: "This is my portfolio",
-  ogTitle: "Portfolio",
-  ogDescription: "This is my portfolio",
-  ogImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
-  ogUrl: "https://mohamed-omar.com",
-  twitterTitle: "Portfolio",
-  twitterDescription: "This is my portfolio",
-  twitterImage: "https://avatars.githubusercontent.com/u/88965473?v=4",
-  twitterCard: "summary",
-});
-
-useHead({
-  htmlAttrs: {
-    lang: "en",
-  },
-  link: [
-    {
-      rel: "icon",
-      type: "image/png",
-      href: "/favicon.png",
-    },
-  ],
-});
-</script>
